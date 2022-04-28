99afc4488965b02df3354137beea67c25c13131c
cc362 <34251554+cc362@users.noreply.github.com>
2022-04-28 12:21:27 -0400

    Update Predictions.py

--- Predictions.py
+++ Predictions.py
@@ -1,67 +1,67 @@
-mport quinn
-import requests
+import random
+import sys
 
-from pyspark.ml.feature import VectorAssembler, Normalizer, StandardScaler
-from pyspark.ml.classification import LogisticRegression
-
+from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType, DoubleType
+from pyspark.sql.functions import col, desc
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.classification import MultilayerPerceptronClassificationModel
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
-from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
-from pyspark.ml import Pipeline
 
-from pyspark.context import SparkContext
-from pyspark.sql.session import SparkSession
+# Create the Spark Session
+spark = SparkSession.builder.appName("CS643-Wine-Quality-Predictions").getOrCreate()
+spark.sparkContext.setLogLevel("ERROR...ABORT")
+print("########## SPARK VER.:", spark.version)
+print("########## SPARK CONTEXT:", spark.sparkContext)
 
-sc = SparkContext('local')
-spark = SparkSession(sc)
 
-train_df = spark.read.format('csv').options(header='true', inferSchema='true', sep=';').load('/app/TrainingDataset.csv')
-test_df = spark.read.format('csv').options(header='true', inferSchema='true', sep=';').load('/app/ValidationDataset.csv')
+# Read the data from the csv file
+print("Reading the data from {}...".format(sys.argv[1]))
+testing = spark.read.format("csv").load(sys.argv[1], header=True, sep=";")
 
-print("Data loaded into Spark.")
-print(train_df.toPandas().head())
+# Change column names and extract feature names.
+testing = testing.toDF("fixed_acidity", "volatile_acidity", "citric_acid", "residual_sugar", "chlorides", "free_sulfur_dioxide", "total_sulfur_dioxide", "density", "pH", "sulphates", "alcohol", "label")
+testing.show(5)
 
-def remove_quotations(s):
-    return s.replace('"', '')
+# Ensure proper data types.
+testing = testing \
+        .withColumn("fixed_acidity", col("fixed_acidity").cast(DoubleType())) \
+        .withColumn("volatile_acidity", col("volatile_acidity").cast(DoubleType())) \
+        .withColumn("citric_acid", col("citric_acid").cast(DoubleType())) \
+        .withColumn("residual_sugar", col("residual_sugar").cast(DoubleType())) \
+        .withColumn("chlorides", col("chlorides").cast(DoubleType())) \
+        .withColumn("free_sulfur_dioxide", col("free_sulfur_dioxide").cast(IntegerType())) \
+        .withColumn("total_sulfur_dioxide", col("total_sulfur_dioxide").cast(IntegerType())) \
+        .withColumn("density", col("density").cast(DoubleType())) \
+        .withColumn("pH", col("pH").cast(DoubleType())) \
+        .withColumn("sulphates", col("sulphates").cast(DoubleType())) \
+        .withColumn("alcohol", col("alcohol").cast(DoubleType())) \
+        .withColumn("label", col("label").cast(IntegerType()))
 
-train_df = quinn.with_columns_renamed(remove_quotations)(train_df)
-train_df = train_df.withColumnRenamed('quality', 'label')
+# Extract feature names. 
+features = testing.columns
+features = features[:-1]
 
-test_df = quinn.with_columns_renamed(remove_quotations)(test_df)
-test_df = test_df.withColumnRenamed('quality', 'label')
+# Convert the read data to the proper feature vector with predicted label format.
+va = VectorAssembler(inputCols=features, outputCol="features")
+va_df = va.transform(testing)
+va_df = va_df.select(["features", "label"])
+testing = va_df
 
-print("Data has been formatted.")
-print(train_df.toPandas().head())
+# Load the model. 
+print("Loading model from {}...".format(sys.argv[2]))
+trModel = MultilayerPerceptronClassificationModel.load(sys.argv[2])
 
-assembler = VectorAssembler(
-    inputCols=["fixed acidity",
-               "volatile acidity",
-               "citric acid",
-               "residual sugar",
-               "chlorides",
-               "free sulfur dioxide",
-               "total sulfur dioxide",
-               "density",
-               "pH",
-               "sulphates",
-               "alcohol"],
-    outputCol="inputFeatures")
+# Make predictions. 
+print("Making predictions...")
+predictions = trModel.transform(testing)
 
-scaler = Normalizer(inputCol="inputFeatures", outputCol="features")
-
-## Only use LogisticRegression because this was the best model decided on
-lr = LogisticRegression()
-
-pipeline1 = Pipeline(stages=[assembler, scaler, lr])
-
-paramgrid = ParamGridBuilder().build()
-
-evaluator = MulticlassClassificationEvaluator(metricName="f1")
-
-crossval = CrossValidator(estimator=pipeline1,
-                          estimatorParamMaps=paramgrid,
-                          evaluator=evaluator,
-                          numFolds=3
-                          )
-
-cvModel1 = crossval.fit(train_df)
-print("F1 Score for Our Model: ", evaluator.evaluate(cvModel1.transform(test_df)))
+# Evaluate the model. 
+print("Evaluating the model...")
+evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="accuracy")
+accuracy = evaluator.evaluate(predictions)
+print("Accuracy = %g " % accuracy)
+evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="f1")
+f1 = evaluator.evaluate(predictions)
+print("F1 = %g " % f1)
+print("Model prediction has completed...going to exit this.")
